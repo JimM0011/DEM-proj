a4f6fa5768b8c5282ef8227fc2f34d9d1ceeab38
Jing Ma <jingma0322@163.com>
2025-10-25 13:44:35 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -23,7 +23,7 @@ export default defineConfig({
       ],
     }),
   ],
-  base:'/academic-project-page-template-vue',
+  base:'/DEM-proj',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
